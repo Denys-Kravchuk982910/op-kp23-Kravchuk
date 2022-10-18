49c7f005556e264604da85d6f8739de742ea8bdd
Denys-Kravchuk-Programmer <deniskravcuk63@gmail.com>
2022-10-18 21:33:52 +0300

    Load homework2 variant 2

---
+++ HomeWork2/Program (2 method).cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Pascal_Triangle
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int rows = 10;
+
+            Console.WriteLine("Pascal's triangle");
+            for (int i = 0; i < rows; i++)
+            {
+                for(int l = rows-i; l > 0; l--)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 0; j <= i; j++)
+                {
+                    int factI = 1;
+                    int factJ = 1;
+                    int divFactIJ = 1;
+
+                    for(int k = 1; k <= i; k++)
+                    {
+                        if(k <= j)
+                        {
+                            factJ *= k;
+                        }
+
+                        if(k <= i - j)
+                        {
+                            divFactIJ *= k;
+                        }
+
+                        factI *=k;
+                    }
+
+
+                    if(factI == 0) { factI = 1; }
+                    if(factJ == 0) { factJ = 1; }
+                    if(divFactIJ == 0) { divFactIJ = 1; }
+
+                    int item = factI / (factJ * divFactIJ);
+
+                    
+
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
