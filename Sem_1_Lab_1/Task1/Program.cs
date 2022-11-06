3d1193ec1d45bd5dd13762295a4a8fbd2ef50c00
Denys Kravchuk <114987963+Denys-Kravchuk982910@users.noreply.github.com>
2022-11-06 14:10:42 +0200

    Add prototypes of programs

---
+++ Sem_1_Lab_1/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task1
+{
+	class Program
+	{
+		static void Main(string[] args) 
+		{
+			double x0 = 0;
+			double xn = 0;
+			double n = 0;
+			int b = -2;
+        }
+
+		static double ResFormula(double x, int b) 
+		{
+			return 0;
+		}
+	}
+}
---
+++ Sem_1_Lab_1/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            uint numberOfFactorial = 0;
+            int x = 1;
+        }
+	}
+}
