cc7bc3605efb519979927b2f3100ced33a2cf33e
Denys Kravchuk <114987963+Denys-Kravchuk982910@users.noreply.github.com>
2022-11-06 14:10:42 +0200

    Add prototypes of programs

---
+++ Sem_1_Lab_1/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            int number = 0;
+        }
+
+		static bool CheckPrime(int number)
+		{
+			return false;
+		}
+	}
+}
---
+++ Sem_1_Lab_1/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task4
+{
+	class Program
+	{
+		static void Main(string[] args) 
+		{
+			int a = 0;
+			int x = 0;
+		}
+
+		static double Sin(int x, int a)
+		{
+			return 0;
+		}
+	}
+}
