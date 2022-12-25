17ddf89e377c83d4819e3b38b9914accbfbf79e6
Denys Kravchuk <114987963+Denys-Kravchuk982910@users.noreply.github.com>
2022-12-25 16:48:51 +0200

    Load stub level of program

--- Sem_1_Lab_2/Task1/Program.cs
+++ Sem_1_Lab_2/Task1/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace Sem_1_Lab_2_Testing
+{
+    class Program
+    {
+        static int N = 10;
+        static int[,] matrix = new int[N, N];
+
+        static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void init(int n)
+        {
+            
+        }
+
+        static void open(int row, int col)
+        {
+            
+        }
+
+        static void connect(int id, int row, int col)
+        {
+
+        }
+
+        static bool find(int[] arr, int index, int last)
+        {
+            return false;
+        }
+
+        static int root(int rowIndex, int colIndex)
+        {
+            return 0;
+        }
+
+        /// <param name="p">new item</param>
+        /// <param name="q">connected item</param>
+        static void union(int p, int q)
+        {
+            
+        }
+
+        static bool isOpen(int row, int col) 
+        {
+            return false;
+        }
+
+        static bool isFull(int row, int col)
+        {
+            return false;
+        }
+
+        static int numberOfOpenSites()
+        {
+            return counter;
+        }
+
+        static bool percolates()
+        {
+            return false;
+        }
+
+        static void print()
+        {
+            
+        }
+    }
+}
