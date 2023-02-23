8527e19c9cb0b073e7c975f1551a61c8576170ac
Denys Kravchuk <114987963+Denys-Kravchuk982910@users.noreply.github.com>
2023-02-23 21:44:37 +0200

    add full realization for task4

--- Sem_2_Lab_1/Task4/Program.cs
+++ Sem_2_Lab_1/Task4/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bogus;
+using System;
+using System.Data;
 using System.Text;
 
 namespace Task4
@@ -14,6 +16,7 @@ namespace Task4
     /// </summary>
     class Program
     {
+        static int MAX_ITEMS_IN_STUDENTS_ARRAY = 10;
         static void Main(string[] args) 
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -22,20 +25,85 @@ namespace Task4
             string directory = Path.Combine(Directory.GetCurrentDirectory(), "data");
             if (!Directory.Exists(directory))
                 Directory.CreateDirectory(directory);
-            string source = Path.Combine(directory, "task3.txt");
+            string source = Path.Combine(directory, "task4.csv");
 
 
             FileInfo file = new FileInfo(source);
+            FillFile(file);
+            DisplayStudentsLessSixtyPoints(file);
         }
 
         static void FillFile(FileInfo info)
         {
+            if (!info.Exists)
+            {
 
+                using (TextWriter sw = new StreamWriter(info.Create(), Encoding.UTF8))
+                {
+                    sw.WriteLine("FirstName,LastName,Age");
+
+                    Faker faker = new Faker("uk");
+                    string name;
+                    string surname;
+                    int age;
+                    for (int i = 0; i < MAX_ITEMS_IN_STUDENTS_ARRAY; i++)
+                    {
+                        var item = faker.Name;
+                        name = item.FirstName(Bogus.DataSets.Name.Gender.Male);
+                        surname = item.LastName(Bogus.DataSets.Name.Gender.Male);
+                        age = faker.Random.Int(0, 100);
+                        sw.WriteLine(name + "," + surname + "," + age);
+                    }
+                }
+            }
         }
 
         static void DisplayStudentsLessSixtyPoints(FileInfo file) 
         {
-        
+            using (StreamReader sr = new StreamReader(file.OpenRead()))
+            {
+                string parameters = sr.ReadLine();
+                char line = (char)sr.Read();
+                int countOfBadStudents = 0;
+                int current = 0;
+                string[] userData = new string[3];
+                while (!sr.EndOfStream)
+                {
+                    int i = 0;
+                    current = 0;
+                    userData[current] = "";
+
+                    while (line != '\n')
+                    {
+                        if(line == ',')
+                        {
+                            userData[current] += " "; 
+                            current++;
+                            userData[current] = "";
+                            line = (char)sr.Read();
+                           
+                            continue;
+                        }
+                        userData[current] += line;
+                        line = (char)sr.Read();
+                    }
+
+                    if (Convert.ToInt64(userData[2]) < 60)
+                    {
+                        Console.WriteLine($"Student {userData[0]} {userData[1]} hasn't enough points to get normal mark!");
+                        countOfBadStudents++;
+                    }
+
+                    line = (char)sr.Read();
+                    
+                }
+
+                if (countOfBadStudents == 0)
+                {
+                    Console.WriteLine("All students have finished semester with enough marks!");
+                }
+            }
         }
     }
+
 }
