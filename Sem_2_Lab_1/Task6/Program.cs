49d160b470c32f256de2224ca9421cfe6cbca4ec
Denys Kravchuk <114987963+Denys-Kravchuk982910@users.noreply.github.com>
2023-02-24 22:15:32 +0200

    add stub level to task6

---
+++ Sem_2_Lab_1/Task6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Buffers.Text;
+
+namespace Task6
+{
+    class Program
+    {
+        /// <summary>
+        /// Task 6.
+        /// Read CSV file that is specified in Task 4, Create a binary file that is based on the CSV file.
+        /// Read the binary file and create another binary file that will contain the number of the
+        /// students whose score is greater than 95 and all the records for those students. 
+        /// </summary>
+        static void Main(string[] args) 
+        {
+            
+        }
+
+        static void CreateFileWithCreatingBinaryOne(FileInfo file) 
+        {
+            
+        }
+
+        static string[] GetUsersByMaximalMark(FileInfo file) 
+        {
+            return new string[] { };
+        }
+    }
+}
