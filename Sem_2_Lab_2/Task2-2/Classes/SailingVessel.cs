6792189ca2f0af92f4144a554a4cccf4f7eea255
Denys Kravchuk <114987963+Denys-Kravchuk982910@users.noreply.github.com>
2023-04-05 13:55:23 +0300

    Load stub level to Task2.2

---
+++ Sem_2_Lab_2/Task2-2/Classes/SailingVessel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2_2.Abstracts;
+
+namespace Task2_2.Classes
+{
+    public class SailingVessel : Vessel
+    {
+        public override void PrepareToMovement()
+        {
+            
+        }
+
+        public override void Move()
+        {
+            
+        }
+    }
+}
---
+++ Sem_2_Lab_2/Task2-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Task2_2.Abstracts;
+using Task2_2.Classes;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+           
+        }
+    }
+}
