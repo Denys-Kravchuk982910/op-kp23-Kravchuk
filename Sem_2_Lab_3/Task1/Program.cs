ad26f37548cc548dd78192e284759806e4a16999
Denys Kravchuk <114987963+Denys-Kravchuk982910@users.noreply.github.com>
2023-05-17 22:14:56 +0300

    Add full realization to task1

--- Sem_2_Lab_3/Task1/Program.cs
+++ Sem_2_Lab_3/Task1/Program.cs
@@ -6,56 +6,152 @@ namespace Task1
     {
         static void Main(string[] args) 
         {
-            
+            Deque<string> queue = new Deque<string>();
+
+            queue.addFirst("string3");
+            queue.addFirst("string2");
+            queue.addFirst("string1");
+            queue.addLast("string4");
+            queue.addLast("string5");
+            queue.addLast("string6");
+
+            queue.removeLast();
+            queue.removeFirst();
+
+            queue.addFirst("string1");
+
+            queue.addLast("string6");
+
+            Console.WriteLine(queue.size());
+
+            Console.WriteLine(queue.isEmpty());
+
+            IIterator<string> it = queue.iterator();
+            while (it.HasNext)
+            {
+                Console.WriteLine(it.MoveNext());
+            }
         }
     }
 
     public class Deque<Item> : IIterator<Item>
     {
-        public bool HasNext => throw new NotImplementedException();
-        private Node<Item> _collection = null;
+        public bool HasNext => _currentItem != null;
+        public Node<Item> _collection;
+        private Node<Item> _currentItem;
         // construct an empty deque
         public Deque()
         {
-            
+            this._collection = null!;
+            this._currentItem = null!;
         }
         
         // is the deque empty?
         public bool isEmpty()
         {
-            return true;    
+            return !this.HasNext;    
         }
         
         // return the number of items on the deque
         public int size()
         {
-            return 0;
+            int counter = 0;
+
+            while(this.HasNext)
+            {
+                counter++;
+                _currentItem = _currentItem.Next;
+            }
+
+
+            _currentItem = _collection;
+            return counter;
         }
         // add the item to the front
         public void addFirst(Item item)
         {
-            
+            //_currentItem = this._collection;
+            if (_collection != null)
+            {
+                Node<Item> newItem = new Node<Item>();
+                newItem.Value = item;
+                newItem.Next = this._collection;
+                this._collection = newItem;
+                return;
+            }
+            this._collection = new Node<Item>();
+            this._currentItem = this._collection;
+            this._collection!.Value = item;
+            this._collection.Next = null!;
         }
         // add the item to the back
         public void addLast(Item item)
         {
+            _currentItem = this._collection;
+            if (_collection != null)
+            {
+                Node<Item> newItem = new Node<Item>();
+                newItem.Value = item;
 
+                var chItem = _currentItem;
+                while (this.HasNext)
+                {
+                    chItem = _currentItem;
+                    _currentItem = _currentItem.Next;
+                }
+
+                chItem.Next = newItem;
+                _currentItem = _collection;
+                return;
+            }
+
+            this._collection = new Node<Item>();
+            this._currentItem = this._collection;
+            this._collection!.Value = item;
+            this._collection.Next = null!;
         }
         
         // remove and return the item from the front
         public Item removeFirst()
         {
-            return default;
+            if (_collection != null) 
+            {
+                var chItem = this._collection.Value;
+                this._currentItem = _collection.Next;
+                this._collection = _collection.Next;
+                return chItem;
+            }
+            return default!;
         }
+
         // remove and return the item from the back
         public Item removeLast()
         {
-            return default;
+            if (_collection != null)
+            {
+                int size = this.size() - 2;
+                for (int i = 0; i < size; i++)
+                {
+                    _currentItem = _currentItem.Next;
+                }
+                _currentItem.Next = null!;
+                _currentItem = _collection;
+
+                if(size == 1)
+                {
+                    _collection = null!;
+                    _currentItem = null!;
+                } 
+                return _currentItem.Value;
+            }
+
+            return default!;
         }
         // return an iterator over items in order from front to back
         public IIterator<Item> iterator()
         {
-            return default;
+            this._currentItem = this._collection;
+            return this;
         }
         // unit testing (required)
         public static void main(String[] args)
@@ -64,7 +160,9 @@ namespace Task1
         }
         public Item MoveNext()
         {
-            throw new NotImplementedException();
+            var val = this._currentItem;
+            this._currentItem = this._currentItem.Next;
+            return val.Value;
         }
     }
     public interface IIterator<T>
@@ -75,8 +173,8 @@ namespace Task1
 
     public class Node<T>
     {
-        public T Value { get; set; } = default;
-        public Node<T> Next { get; set; } = null;
+        public T Value { get; set; } = default!;
+        public Node<T> Next { get; set; } = null!;
     }
 
 
