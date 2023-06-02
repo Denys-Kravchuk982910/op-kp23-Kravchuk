5b1edd54fd6bcd957ca8a72f183add50274d24eb
Denys Kravchuk <114987963+Denys-Kravchuk982910@users.noreply.github.com>
2023-06-02 16:43:34 +0300

    Add errors handling

--- Coursework/TotalCommander/Program.cs
+++ Coursework/TotalCommander/Program.cs
@@ -10,28 +10,43 @@ namespace TotalCommander
     {
        static void Main(string[] args) 
        {
-            Console.CursorVisible = false;
-            Console.OutputEncoding = Encoding.Unicode;
-            Console.InputEncoding = Encoding.Unicode;
-            Console.SetWindowSize((int)((double)Console.LargestWindowWidth / 1.2),
-                (int)((double)Console.LargestWindowHeight / 1.2));
+            try
+            {
 
-            DesignOfWindow.MakeLogo();
-            DesignOfWindow.MakeBackground();
-            DesignOfWindow.MakeBorders();
+                Console.CursorVisible = false;
+                Console.OutputEncoding = Encoding.Unicode;
+                Console.InputEncoding = Encoding.Unicode;
+                Console.SetWindowSize((int)((double)Console.LargestWindowWidth / 1.2),
+                    (int)((double)Console.LargestWindowHeight / 1.2));
 
-            List<string> options = new List<string> {
+                DesignOfWindow.MakeLogo();
+                DesignOfWindow.MakeBackground();
+                DesignOfWindow.MakeBorders();
+
+                List<string> options = new List<string> {
                 "M - create directoy",
                 "F - create file",
                 "T - delete current item",
             };
 
-            DesignOfWindow.MakePointers(options);
+                DesignOfWindow.MakePointers(options);
 
 
-            MainMenu menu = new MainMenu();
+                MainMenu menu = new MainMenu();
 
-            menu.StartApplication();
+                menu.StartApplication();
+            }
+            catch (Exception ex)
+            {
+                string message = "***\nSource: " + ex.Source + "\nMessage: " + ex.Message
+                    + "\nStackTrace:" + ex.StackTrace + "\n***";
+                StreamWriter sw = File.AppendText(Path.Combine(Directory.GetCurrentDirectory(),
+                    "Local", "logs.txt"));
+
+                sw.WriteLine(message);
+
+                sw.Dispose();
+            }
         }
 
     }
